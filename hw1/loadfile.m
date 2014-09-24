bb595ad8b321f072b677165ce094e33ffdbc9e9e
chengliang dong <chengliangdong@usc-secure-wireless-016-023.usc.edu>
2014-09-23 20:26:02 -0700

    fix laod

--- hw1/loadfile.m
+++ hw1/loadfile.m
@@ -2,12 +2,12 @@ function [train_data, train_label, test_data, test_label, validation_data, valid
 
 % load files
 
-train_data_file = "car_train.data.out.x"
-train_label_file = "car_train.data.out.y"
-test_data_file = "car_test.data.out.x"
-test_label_file = "car_test.data.out.y"
-validation_data_file = "car_valid.data.out.x"
-validation_label_file = "car_valid.data.out.y"
+train_data_file = 'car_train.data.out.x'
+train_label_file = 'car_train.data.out.y'
+test_data_file = 'car_test.data.out.x'
+test_label_file = 'car_test.data.out.y'
+validation_data_file = 'car_valid.data.out.x'
+validation_label_file = 'car_valid.data.out.y'
 
 
 train_data = importdata(train_data_file)
