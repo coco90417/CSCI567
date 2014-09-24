eb28022769f47d366665d442e4298caf83074b2e
chengliang dong <chengliangdong@chengliangs-MacBook-Air.local>
2014-09-24 07:12:53 -0700

    plots!

--- hw1/boundary.m
+++ hw1/boundary.m
@@ -7,33 +7,31 @@ boundary = matfile('boundary.mat');
 
 train_data = boundary.features;
 train_label = boundary.labels;
-new_data = (1:100, 1:100);
-new_data = new_data * 0.01-0.005;
 
 for j = 1:100
-temp_matrix = ones(2,100);
-
-
+temp_matrix = ones(100,2)* 0.01 * j-0.005;
+temp_matrix(1:100,2) = [1:100]* 0.01-0.005;
+new_data = temp_matrix;
 total_data = [train_data; new_data];
 distance = pdist(total_data);
 distanceMatrix = squareform(distance);
 distanceMatrix = distanceMatrix((size(train_data,1)+1):size(total_data,1), 1:size(train_data,1));
 sortedDistance = sort(distanceMatrix, 2);
-
 for i=1:size(new_data,1)
 tempIndex = find(distanceMatrix(i,:)<=sortedDistance(i,k));
 finalIndexTest(i,1:k)= tempIndex(1:k);
 end
 finalIndexTest=finalIndexTest(:,1:k);
-
 estimatedClass=mode(train_label(finalIndexTest),2);
-new_accu = size(new_data(estimatedClass==new_label),1)/size(new_data,1);
+plot_class(j:(j+99)) = estimatedClass;
+x(j:(j+99)) = ones(100,1)* 0.01 * j-0.005;
+y(j:(j+99)) = [1:100]* 0.01-0.005;
+s(j:(j+99)) = ones(100,1) * 20;
 
-
+end
 
 h = figure;
-s=0.1;
-c=boundary
+c=plot_class*1000-2000;
 scatter(x,y,s,c,'fill')
 saveas(h, filename);
                               
