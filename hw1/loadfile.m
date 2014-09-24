2dcb50783b8b13de67fcbc9c258997f2be30d863
chengliang dong <chengliangdong@usc-secure-wireless-016-023.usc.edu>
2014-09-23 21:49:32 -0700

    naive bayes

--- hw1/loadfile.m
+++ hw1/loadfile.m
@@ -2,18 +2,18 @@ function[train_data, train_label, test_data, test_label, validation_data, valida
 
 % load files
 
-train_data_file = 'car_train.data.out.x'
-train_label_file = 'car_train.data.out.y'
-test_data_file = 'car_test.data.out.x'
-test_label_file = 'car_test.data.out.y'
-validation_data_file = 'car_valid.data.out.x'
-validation_label_file = 'car_valid.data.out.y'
+train_data_file = 'car_train.data.out.x';
+train_label_file = 'car_train.data.out.y';
+test_data_file = 'car_test.data.out.x';
+test_label_file = 'car_test.data.out.y';
+validation_data_file = 'car_valid.data.out.x';
+validation_label_file = 'car_valid.data.out.y';
 
 
-train_data = importdata(train_data_file)
-train_label = importdata(train_label_file)
-test_data = importdata(test_data_file)
-test_label = importdata(test_label_file)
-validation_data = importdata(validation_data_file)
-validation_label = importdata(validation_label_file)
+train_data = importdata(train_data_file);
+train_label = importdata(train_label_file);
+test_data = importdata(test_data_file);
+test_label = importdata(test_label_file);
+validation_data = importdata(validation_data_file);
+validation_label = importdata(validation_label_file);
 
--- hw1/naive_bayes.m
+++ hw1/naive_bayes.m
@@ -13,3 +13,24 @@ function [new_accu, train_accu] = naive_bayes(train_data, train_label, new_data,
 %  train_accu: accuracy of classifying train_data 
 %
 % CSCI 576 2014 Fall, Homework 1
+
+% trainmodel
+xparameters = [sum(train_data(train_label==1,:))/size(train_label(train_label==1),1); sum(train_data(train_label==2,:))/size(train_label(train_label==2),1); sum(train_data(train_label==3,:))/size(train_label(train_label==3),1); sum(train_data(train_label==4,:))/size(train_label(train_label==4),1)];
+xparameters(xparameters==0)=0.1;
+yparameters = [size(train_label(train_label==1),1)/size(train_label,1), size(train_label(train_label==2),1)/size(train_label,1), size(train_label(train_label==3),1)/size(train_label,1), size(train_label(train_label==4),1)/size(train_label,1)];
+logXparameters = log(xparameters);
+logYparameters = log(xparameters);
+
+%  train_accu: accuracy of classifying train_data
+logEstimated = (train_data * transpose(logXparameters)+ repmat(logYparameters,size(train_label,1),1));
+estimated = exp(logEstimated);
+[estimatedMaxVal estimatedMaxInd] = max(estimated,[],2);
+size(train_data(estimatedMaxInd==train_label),1);
+train_accu = size(train_data(estimatedMaxInd==train_label),1)/size(train_data,1);
+
+%  new_accu: accuracy of classifying new_data
+logEstimated = (test_data * transpose(logXparameters)+ repmat(logYparameters,size(test_label,1),1));
+estimated = exp(logEstimated);
+[estimatedMaxVal estimatedMaxInd] = max(estimated,[],2);
+size(test_data(estimatedMaxInd==test_label),1);
+new_accu = size(test_data(estimatedMaxInd==test_label),1)/size(test_data,1);
