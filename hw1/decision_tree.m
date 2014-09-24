2fb094dbb7b96065b73450fc6d16d81d721cde81
chengliang dong <chengliangdong@chengliangs-MacBook-Air.local>
2014-09-24 05:08:18 -0700

    decision tree

--- hw1/decision_tree.m
+++ hw1/decision_tree.m
@@ -5,8 +5,8 @@ function [new_accu_1, train_accu_1, new_accu_2, train_accu_2] = decision_tree(tr
 tree = ClassificationTree.fit(train_data, train_label, 'MinLeaf', i, 'SplitCriterion', 'gdi', 'Prune', 'off');
 tree2 = ClassificationTree.fit(train_data, train_label, 'MinLeaf', i, 'SplitCriterion', 'deviance', 'Prune', 'off');
 
-pred_tree1_train = tree1.predict(new_data);
-pred_tree2_train = tree2.predict(new_data);
+pred_tree1_train = tree1.predict(train_data);
+pred_tree2_train = tree2.predict(train_data);
                               
 pred_tree1_new = tree1.predict(new_data);
 pred_tree2_new = tree2.predict(new_data);
