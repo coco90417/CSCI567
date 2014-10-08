535d2fdc4b306a18baeada031137eb4d95b25512
chengliang dong <chengliangdong@chengliangs-MacBook-Air.local>
2014-10-08 07:32:52 -0700

    hw2

--- hw2/gradientdescent.m
+++ hw2/gradientdescent.m
@@ -13,16 +13,24 @@ new_test_data = [ones(size(test_data, 1),1) test_data];
 lambda = 0:0.05:0.5;
 stepsize = [0.001, 0.01, 0.05, 0.1, 0.5];
 T = 1:1:50;
+sigma = 1./(1 + exp(-(new_train_data * new_w)));
+
 
 % run
 for i = 1:size(lambda,2)
-for j = 1:size(stepsize)
-for k = 1:size(T)
-    gradient = transpose(new_train_data) * (1./(1 + exp(-(new_train_data * new_w)))-train_label) + 2*lambda(i)*lambda_w;
+for j = 1:size(stepsize,2)
+for k = 1:size(T,2)
+    gradient = transpose(new_train_data) * (sigma-train_label) + 2*lambda(i)*lambda_w;
     new_w = new_w - stepsize(j) * gradient;
+    sigma_train = 1./(1 + exp(-(new_train_data * new_w)));
+    sigma_test = 1./(1 + exp(-(new_test_data * new_w)));
     lambda_w = [0; new_w(2:size(new_w,1))];
-    train_data_cross_entropy(i, j, k) = transpose(train_label) * log(1./(1 + exp(-(new_train_data * new_w)))) + transpose(ones(size(train_label,1),1)-train_label) * log(1-(1./(1 + exp(-(new_train_data * new_w))))) + 2*lambda(i)*norm(lambda_w);
-    test_data_cross_entropy(i, j, k) = transpose(test_label) * log(1./(1 + exp(-(new_test_data * new_w)))) + transpose(ones(size(test_label,1),1)-test_label) * log(1-(1./(1 + exp(-(new_test_data * new_w))))) + 2*lambda(i)*norm(lambda_w);
+    sigma_train(sigma_train>1-1e-16) = 1-1e-16;
+    sigma_test(sigma_test>1-1e-16) = 1-1e-16;
+    sigma_train(sigma_train>1-1e-16) = 1-1e-16;
+    sigma_test(sigma_test>1-1e-16) = 1-1e-16;
+    train_data_cross_entropy(i, j, k) = transpose(train_label) * log(sigma_train) + transpose(ones(size(train_label,1),1)-train_label) * log(ones(size(train_label,2),1)-sigma_train) + 2*lambda(i)*norm(lambda_w)^2;
+    test_data_cross_entropy(i, j, k) = transpose(test_label) * log(sigma_test) + transpose(ones(size(test_label,1),1)-test_label) * log(ones(size(train_label,2),1)-sigma_test) + 2*lambda(i)*norm(lambda_w)^2;
     l2norm(i, j, k) = norm(lambda_w);
 end
 end
