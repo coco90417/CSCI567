2f86e7d392cc6203089cece61725c91777fa8d40
chengliang dong <chengliangdong@chengliangs-MacBook-Air.local>
2014-10-08 12:23:19 -0700

    hw2

--- hw2/gradientdescent.m
+++ hw2/gradientdescent.m
@@ -33,7 +33,7 @@ for k = 1:size(T,2)
     sigma_train(sigma_train<10^(-16)) = 10^(-16);
     sigma_test(sigma_test<10^(-16)) = 10^(-16);
     train_data_cross_entropy(i, j, k) = -(transpose(train_label) * log(sigma_train) + transpose(ones(size(train_label,1),1)-train_label) * log(ones(size(train_label,1),1)-sigma_train)) + lambda(i)*norm(w)^2;
-    test_data_cross_entropy(i, j, k) = transpose(test_label) * log(sigma_test) + transpose(ones(size(test_label,1),1)-test_label) * log(ones(size(train_label,1),1)-sigma_test) + lambda(i)*norm(w)^2;
+    test_data_cross_entropy(i, j, k) = -(transpose(test_label) * log(sigma_test) + transpose(ones(size(test_label,1),1)-test_label) * log(ones(size(test_label,1),1)-sigma_test)) + lambda(i)*norm(w)^2;
     l2norm(i, j, k) = norm(w);
 end
 end
