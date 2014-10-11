fc7f416db86a339cccc108ec0f8cc2eee655eba7
chengliang dong <chengliangdong@usc-secure-wireless-017-001.usc.edu>
2014-10-10 20:38:08 -0700

    hw3

---
+++ hw3/loadfile.m
@@ -0,0 +1,9 @@
+function[train_data, train_label, test_data, test_label] = loadfile()
+
+% load files
+train_data_file = 'splice_test.mat';
+train_data = importdata(train_data_file);
+
+test_data_file = 'splice_train.mat';
+test_data = importdata(test_data_file);
+
