5993973232fefa3fefee414c8e57ebcfb5094225
chengliang dong <chengliangdong@usc-secure-wireless-017-001.usc.edu>
2014-10-10 23:43:31 -0700

    hw3

--- hw3/CSCI567_hw3.m
+++ hw3/CSCI567_hw3.m
@@ -1,9 +1,6 @@
 [train_data, train_label, test_data, test_label] = loadfile();
 
 % load files
-train_data_file = 'splice_test.mat';
-train_data = importdata(train_data_file);
 
-test_data_file = 'splice_train.mat';
-test_data = importdata(test_data_file);
+model = svmtrain(train_label, train_data, '-t 0 -c 4^(−6) -v 5');
 
