2163138ebc91800a5a386b31ea79c4e3723dfc4c
chengliang dong <chengliangdong@chengliangs-MacBook-Air.local>
2014-10-14 02:16:58 -0700

    hw3

--- hw3/CSCI567_hw3.m
+++ hw3/CSCI567_hw3.m
@@ -5,36 +5,6 @@
 % add path, required for libsvm
 addpath('/home/cocodong/usr/matlab/lib/libsvm-3.18/matlab');
 
-[m n] = size(train_data);
-[test_m test_n] = size(test_data);
-
-
-crosssize = round(m/5);
-sub_test_data(1) = train_data(1:crosssize, :);
-sub_test_label(1) = train_label(1:crosssize, :);
-sub_train_data(1) = set_diff(train_data, sub_test_data(1), 'row');
-sub_train_label(1) = set_diff(train_label, sub_train_label(1), 'row');
-
-sub_test_data(2) = train_data(crosssize+1:2*crosssize, :);
-sub_test_label(2) = train_label(crosssize+1:2*crosssize, :);
-sub_train_data(2) = set_diff(train_data, sub_test_data(2), 'row');
-sub_train_label(2) = set_diff(train_label, sub_train_label(2), 'row');
-
-sub_test_data(3) = train_data(2*crosssize+1:3*crosssize, :);
-sub_test_label(3) = train_label(2*crosssize+1:3*crosssize, :);
-sub_train_data(3) = set_diff(train_data, sub_test_data(3), 'row');
-sub_train_label(3) = set_diff(train_label, sub_train_label(3), 'row');
-
-sub_test_data(4) = train_data(3*crosssize+1:4*crosssize, :);
-sub_test_label(4) = train_label(3*crosssize+1:4*crosssize, :);
-sub_train_data(4) = set_diff(train_data, sub_test_data(4), 'row');
-sub_train_label(4) = set_diff(train_label, sub_train_label(4), 'row');
-
-sub_test_data(5) = train_data(4*crosssize:m, :);
-sub_test_label(5) = train_label(5*crosssize:m, :);
-sub_train_data(5) = set_diff(train_data, sub_test_data(5), 'row');
-sub_train_label(5) = set_diff(train_label, sub_train_label(5), 'row');
-
 % implement linear SVM
 cost = [4^(-6) 4^(-5) 4^(-4) 4^(-3) 4^(-2) 4^(-1) 1 4 4^2];
 
@@ -42,15 +12,23 @@ for i=1:9
 tempclock = clock();
 starttime = tempclock(5) * 60 + tempclock(6);
 temp_accuracy = zeros(5,1);
-for j=1:5
-[temp_w,temp_b] = trainsvm(sub_train_data(j), sub_train_label(j), cost(i));
-temp_accuracy(j) = testsvm(sub_test_data(j), sub_test_label(j), temp_w, temp_b);
+
+indices = crossvalind('Kfold',train_label,5);
+for j = 1:5
+test = (indices == i); train = ~test;
+temp_train_data = train_data(train,:);
+temp_train_label = train_label(train);
+temp_test_data = train_data(test,:);
+temp_test_label = train_label(test);
+[temp_w temp_b] = trainsvm(temp_train_data,temp_train_label, cost(i));
+temp_accuracy(j) = testsvm(temp_test_data,temp_test_label, temp_w, temp_b);
 end
+
 tempclock = clock();
 endtime  = tempclock(5) * 60 + tempclock(6);
 second = endtime  - starttime ;
 average_time(i) = second/5;
-accuracy = sum(temp_accuracy)/5;
+accuracy(i) = sum(temp_accuracy)/5;
 end
  
 % Use linear SVM in LIBSVM
@@ -58,9 +36,9 @@ model = zeros(9,1);
 tempclock = clock();
 starttime = tempclock(5) * 60 + tempclock(6);
 model(1) = svmtrain(train_label, train_data, '-t 0 -c 4^(−6) -v 5');
-model(2) = svmtrain(train_label, train_data, '-t 0 -c 4^(−5) -v 5');
+model_two = svmtrain(train_label, train_data, '-t 0 -c 4^(−5) -v 5');
 model(3) = svmtrain(train_label, train_data, '-t 0 -c 4^(−4) -v 5');
-model(4) = svmtrain(train_label, train_data, '-t 0 -c 4^(−3) -v 5');
+model_four = svmtrain(train_label, train_data, '-t 0 -c 4^(−3) -v 5');
 model(5) = svmtrain(train_label, train_data, '-t 0 -c 4^(−2) -v 5');
 model(6) = svmtrain(train_label, train_data, '-t 0 -c 4^(−1) -v 5');
 model(7) = svmtrain(train_label, train_data, '-t 0 -c 4^(0) -v 5');
@@ -80,7 +58,7 @@ model(1) = svmtrain(train_label, train_data, '-t 1 -c 4^(−3) -d 1 -v 5');
 model(2) = svmtrain(train_label, train_data, '-t 1 -c 4^(−2) -d 1 -v 5');
 model(3) = svmtrain(train_label, train_data, '-t 1 -c 4^(−1) -d 1 -v 5');
 model(4) = svmtrain(train_label, train_data, '-t 1 -c 4^(0) -d 1 -v 5');
-model(5) = svmtrain(train_label, train_data, '-t 1 -c 4^(1) -d 1 -v 5');
+model_five = svmtrain(train_label, train_data, '-t 1 -c 4^(1) -d 1 -v 5');
 model(6) = svmtrain(train_label, train_data, '-t 1 -c 4^(2) -d 1 -v 5');
 model(7) = svmtrain(train_label, train_data, '-t 1 -c 4^(3) -d 1 -v 5');
 model(8) = svmtrain(train_label, train_data, '-t 1 -c 4^(4) -d 1 -v 5');
