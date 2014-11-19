4a64a229040681e5ab71bf6771d7e3b81dbc57aa
chengliang dong <chengliangdong@chengliangs-MacBook-Air.local>
2014-11-19 02:26:49 -0800

    hw4

--- hw4/CSCI567_hw4.m
+++ hw4/CSCI567_hw4.m
@@ -4,10 +4,10 @@
 [train_data] = loadfile();
 
 % k = 2, 3, 5
-k = [2,3,5];
+k_vector = [2,3,5];
 for i=1:3
-[train_data, cost_vector] = kmeans(train_data, k(i));
-name = strcat('scatter plot of k means clustering with k= ', num2str(k(i)));
-filename = strcat('qustion1_', num2str(k(i)), '.pdf');
-plot(train_data, name, filename);
+[train_data, cost_vector] = kmeans(train_data, k_vector(i));
+name = strcat('scatter plot of k means clustering with k= ', num2str(k_vector(i)));
+filename = strcat('qustion1_', num2str(k_vector(i)), '.pdf');
+plotcluster(train_data, name, filename);
 end
