e1c3b8669940d6d98770e355e371996498611e9a
chengliang dong <chengliangdong@chengliangs-MacBook-Air.local>
2014-11-19 09:51:44 -0800

    hw4

--- hw4/kmeans.m
+++ hw4/kmeans.m
@@ -3,28 +3,29 @@ function [output_label, cost_vector] = kmeans(train_data, train_label, k)
 [m n] = size(train_data);
 mu = rand(k,n-1);
 iteration=0;
-class = zeros(m, 1);
+output_label = zeros(m, 1);
+class = train_label;
 
 while(isequaln(class, output_label))
 iteration=iteration+1;
 cost_vector(iteration) = 0;
-class = train_data(:,1);
+class = output_label;
 for i = 1:m
-observation = train_data(i, 2:3);
+observation = train_data(i, :);
 copy_observation = repmat(observation, k, 1);
 distance = sum((copy_observation-mu).^2, 2);
 [min_distance, index] = min(distance);
-train_data(i, 1) = index;
+output_label = index;
 end
 
 for i = 1:k
-mu(i,:) = sum(train_data(train_data(:,1)==i, 2:3))/sum(train_data(train_data(:,1)==i));
+mu(i,:) = sum(train_data(output_label==i, :))/sum(output_label==i));
 end
 
 for i = 1:m
-observation = train_data(i, 2:3);
-index = train_data(i, 1);
-cost_vector(iteration) = cost_vector(iteration) + dot((observation-mu(index,:))', (observation-mu(index,:))');
+observation = train_data(i, :);
+index = output_label(i);
+cost_vector(iteration) = cost_vector(iteration) + sum((observation-mu(index,:)).^2, 2);
 end
 
 end
