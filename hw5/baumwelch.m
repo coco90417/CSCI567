c1df86ad42e0220fc23a4f8bf194c0cf5f55333c
chengliang dong <chengliangdong@usc-secure-wireless-019-014.usc.edu>
2014-11-30 20:07:49 -0800

    hw5

--- hw5/baumwelch.m
+++ hw5/baumwelch.m
@@ -13,51 +13,56 @@ function [A_estimate, E_estimate] = baumwelch(data, A_guess, E_guess, N_iter)
 % CSCI 576 2014 Fall, Homework 5
 
 
-A_guess = [0.7 0.3;0.3 0.7];
-B_guess = [0.25 0.25 0.25 0.25;0.25 0.25 0.25 0.25];
-pi = [0.8 0.2];
+A_guess = [0.7 0.3; 0.3 0.7];
+B_guess = [0.25 0.25 0.25 0.25; 0.25 0.25 0.25 0.25];
+pi = [0.1 0.9];
+alpha = zeros(6, 2, size(data,1));
+beta = zeros(6, 2, size(data,1));
+
 my_N_iter = 1;
 
 while( my_N_iter <= N_iter)
 
 % alpha(time, state, sample)
 % initialization
-for n = 1:1000
-for i = 1:2
+for n = 1:size(data, 1)
+    for i = 1:2
     alpha(1,i,n) = pi(i)*B_guess(i,data(n,1));
-end
+    end
     c(1,n) = 1/sum(alpha(1,:,n));
     for i = 1:2
-    alpha(1,i,n) = c(1,n)*alpha(1,i,n);
+    alpha(1,i,n) = c(1,n)* alpha(1,i,n);
+    end
 end
 
 % update
 for t=2:6
-for n=1:1000
+for n=1:size(data, 1)
 for i=1:2
-alpha(t,i,n) = sum(alpha(t-1,:,n).*A_guess(:,i).*B_guess(i,data(n,t)));
+alpha(t,i,n) = sum(alpha(t-1,:,n)'.* A_guess(:,i).* B_guess(i,data(n,t)));
 end
-    c(t,n) = 1/sum(alpha(t,:,n));
+c(t,n) = 1/sum(alpha(t,:,n));
 for i = 1:2
-    alpha(t,i,n) = c(t,n)*alpha(t,i,n);
+alpha(t,i,n) = c(t,n)*alpha(t,i,n);
+end
+end
 end
 
 % beta(time, state, sample)
 % initialization
-for n = 1:1000
+for n = 1:size(data, 1)
 for i = 1:2
-beta(6,i,n) = 1;
-beta(6,i,n) = c(6,n)*beta(6,i,n);
+beta(6,i,n) = c(6,n);
 end
 end
 
 % update
 for t=1:5
-for n=1:1000
+for n=1:size(data, 1)
 for i=1:2
 beta_t = 6-t;
-beta(beta_t,i,n) = sum(A_guess(i,:).*B_guess(:,data(n,beta_t+1)));
-beta(beta_t,i,n) = c(beta_t,n)*beta(beta_t,i,n);
+beta(beta_t,i,n) = sum(A_guess(i,:)'.* B_guess(:,data(n,beta_t+1)).* beta(beta_t+1,:,n)');
+beta(beta_t,i,n) = c(beta_t,n) * beta(beta_t,i,n);
 end
 end
 end
@@ -65,37 +70,44 @@ end
 % parameter
 
 A_estimate = zeros(2,2);
+B_estimate = zeros(2,4);
 for i = 1:2
 for j = 1:2
 A_estimate_temp_up = 0;
 A_estimate_temp_down = 0;
-for m = 1:1000
-A_estimate_temp_up(m) = sum(alpha(1:5,i,m).*A_guess(i,j).*B_guess(j,data(m,2:6)).*beta(2:6,j,m));
-A_estimate_temp_down(m) = sum(alpha(1:5,i,m).*beta(i,j,m));
+for m = 1:size(data, 1)
+A_estimate_temp_up(m) = sum(alpha(1:5,i,m)*A_guess(i,j).*B_guess(j,data(m,2:6))'.*beta(2:6,j,m));
+A_estimate_temp_down(m) = sum(alpha(1:5,i,m)*beta(i,j,m)./c(1:5,m));
 end
 A_estimate(i,j) = sum(A_estimate_temp_up)/sum(A_estimate_temp_down);
 end
 end
 
 B_estimate = zeros(2,4);
+B_estimate_temp_down_one = zeros(size(data, 1),1);
+
 for i = 1:2
-for m = 1:size(data(:,k)==k,1)
+for m = 1:size(data, 1)
 temp_alpha_one = alpha(data(m,:)==1,i,m);
 temp_beta_one = beta(data(m,:)==1,i,m);
+c_one = c(data(m,:)==1,m);
 temp_alpha_two = alpha(data(m,:)==2,i,m);
 temp_beta_two = beta(data(m,:)==2,i,m);
+c_two = c(data(m,:)==2,m);
 temp_alpha_three = alpha(data(m,:)==3,i,m);
 temp_beta_three = beta(data(m,:)==3,i,m);
+c_three = c(data(m,:)==3,m);
 temp_alpha_four = alpha(data(m,:)==4,i,m);
 temp_beta_four = beta(data(m,:)==4,i,m);
-B_estimate_temp_up_one(m) = sum(temp_alpha_one(:,i,m).*temp_beta_one(:,i,m));
-B_estimate_temp_up_two(m) = sum(temp_alpha_two(:,i,m).*temp_beta_two(:,i,m));
-B_estimate_temp_up_three(m) = sum(temp_alpha_three(:,i,m).*temp_beta_three(:,i,m));
-B_estimate_temp_up_four(m) = sum(temp_alpha_four(:,i,m).*temp_beta_four(:,i,m));
-B_estimate_temp_down_one(m) = sum(alpha_one(:,i,m).*beta_one(:,i,m));
-B_estimate_temp_down_two(m) = sum(alpha_one(:,i,m).*beta_one(:,i,m));
-B_estimate_temp_down_three(m) = sum(alpha_one(:,i,m).*beta_one(:,i,m));
-B_estimate_temp_down_four(m) = sum(alpha_one(:,i,m).*beta_one(:,i,m));
+c_four = c(data(m,:)==4,m);
+B_estimate_temp_up_one(m) = sum(temp_alpha_one.*temp_beta_one ./c_one);
+B_estimate_temp_up_two(m) = sum(temp_alpha_two.*temp_beta_two ./c_two);
+B_estimate_temp_up_three(m) = sum(temp_alpha_three.*temp_beta_three ./c_three);
+B_estimate_temp_up_four(m) = sum(temp_alpha_four.*temp_beta_four ./c_four);
+B_estimate_temp_down_one(m) = sum(alpha(:,i,m).*beta(:,i,m)./c(:,m));
+B_estimate_temp_down_two(m) = sum(alpha(:,i,m).*beta(:,i,m)./c(:,m));
+B_estimate_temp_down_three(m) = sum(alpha(:,i,m).*beta(:,i,m)./c(:,m));
+B_estimate_temp_down_four(m) = sum(alpha(:,i,m).*beta(:,i,m)./c(:,m));
 end
 B_estimate(i,1) = sum(B_estimate_temp_up_one)/sum(B_estimate_temp_down_one);
 B_estimate(i,2) = sum(B_estimate_temp_up_two)/sum(B_estimate_temp_down_two);
@@ -106,7 +118,10 @@ end
 
 A_guess = A_estimate;
 B_guess = B_estimate;
+my_N_iter = my_N_iter + 1;
 end
+                            
+                            
 
 
 
